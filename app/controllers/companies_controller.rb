470e0edb3affa175a9380a4afc84ab4828705a62
Pyatov Nikita <ctulhuweb@gmail.com>
2020-09-24 17:12:57 +0700

    added order by name

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,7 +3,7 @@ class CompaniesController < ApplicationController
     tags_subquery = Company.select("companies.name, companies.id,  string_agg(tags.name, ',') as total_tags").joins(:tags).group(:id).to_sql
     contacts_subquery = Company.select('companies.id, count(contacts.id) as contacts_count').joins(:contacts).where(contacts: {user_id: permit_params[:user_id]}).group(:id).to_sql  
     
-    @companies = Company.select('company_tags.name, total_tags, contacts_count').from("(#{contacts_subquery}) as total_contacts, (#{tags_subquery}) as company_tags").where("total_contacts.id = company_tags.id")
+    @companies = Company.select('company_tags.name, total_tags, contacts_count').from("(#{contacts_subquery}) as total_contacts, (#{tags_subquery}) as company_tags").where("total_contacts.id = company_tags.id").order("company_tags.name")
     render json: @companies.as_json(except: :id)
   end
 
